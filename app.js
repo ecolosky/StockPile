0e07eeca722def8e0ec2ee2e0f6d958f0630bd27
Edward Colosky <edwardcolosky628@gmail.com>
2016-05-05 23:14:50 -0400

    0.1.0 - working nav, template of list layout

--- app.js
+++ app.js
@@ -7,46 +7,78 @@
 
 var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
 // configure the routes
-    // app.config(function($routeProvider) {
-    //     $routeProvider
-    //
-    //         // route for the home page
-    //         .when('/', {
-    //             templateUrl : 'pages/home.html',
-    //             controller  : 'mainCtrl'
-    //         })
-    //
-    //         // route for the gallery page
-    //         .when('/gallery', {
-    //             templateUrl : 'pages/gallery.html',
-    //             controller  : 'galleryCtrl'
-    //         })
-    //
-    //         // route for the stories page
-    //         .when('/stories', {
-    //             templateUrl : 'pages/stories.html',
-    //             controller : 'storyCtrl'
-    //         })
-    //         // route for the chapter history
-    //         .when('/ChapterHistory', {
-    //             templateUrl : 'pages/ChapterHistory/index.html'
-    //         })
-    //         // route for the stories page
-    //         .when('/links', {
-    //             templateUrl : 'pages/links.html'
-    //         });
-    // });
+    app.config(function($routeProvider) {
+        $routeProvider
 
+            // route for the home page
+            .when('/inventory', {
+                templateUrl : 'pages/inventory.html',
+                controller  : 'inventoryCtrl'
+            })
+
+            // route for the gallery page
+            .when('/gallery', {
+                templateUrl : 'pages/gallery.html',
+                controller  : 'galleryCtrl'
+            })
+
+            // route for the stories page
+            .when('/stories', {
+                templateUrl : 'pages/stories.html',
+                controller : 'storyCtrl'
+            })
+            // route for the chapter history
+            .when('/ChapterHistory', {
+                templateUrl : 'pages/ChapterHistory/index.html'
+            })
+            // route for the stories page
+            .when('/links', {
+                templateUrl : 'pages/links.html'
+            });
+    });
+    //main controller
     app.controller('mainCtrl',function($scope){
 
       $scope.tabs = [
-        { title:"Inventory", href:"#/", icon: 'glyphicon glyphicon-list' },
-        { title:"Shopping List", href:"#/gallery" ,icon: 'glyphicon glyphicon-shopping-cart' },
-        { title:"Add Item", href:"#/stories" ,icon: 'glyphicon glyphicon-plus' },
-        { title:"Nuitrition", href:"#/ChapterHistory", icon: 'glyphicon glyphicon-scale'  },
-        { title:"Reipes", href:"#/links", icon: 'glyphicon glyphicon-cutlery'  }
+        { title:"Inventory", href:"#/inventory", icon: 'glyphicon glyphicon-list' },
+        { title:"Shopping List", href:"#/list" ,icon: 'glyphicon glyphicon-shopping-cart' },
+        { title:"Add Item", href:"#/add" ,icon: 'glyphicon glyphicon-plus' },
+        { title:"Nuitrition", href:"#/nuitrition", icon: 'glyphicon glyphicon-scale'  },
+        { title:"Recipes", href:"#/recipes", icon: 'glyphicon glyphicon-cutlery'  }
       ];
-      // $scope.changeTab = function(data) {
-      //   window.location.hash = data;
-      // };
+      $scope.changeTab = function(data) {
+        window.location.hash = data.href;
+        $scope.title = data.title;
+      };
+    });
+    app.controller('inventoryCtrl',function($scope){
+      $scope.title = "Inventory";
+      $scope.inventory = [
+        {head: "Produce",
+          items: [
+            {item: "apples", count: "2"},
+            {item: "bannannas", count: "6"},
+            {item: "oranges", count: "1"},
+            {item: "strawberries", count: "1 Qt."}
+          ]
+        },
+        {head: "Meat",
+          items: [
+            {item: "chicken", count: "5 lbs"},
+            {item: "pork tenderloin", count: "3 lbs"},
+            {item: "bacon", count: "1.5 lbs"},
+            {item: "chicken sausage", count: "5 links"}
+          ]
+        },
+        {head: "Beverages",
+          items: [
+            {item: "beer", count: "2.5 gal"},
+            {item: "wine", count: "2.25 L"},
+            {item: "milk", count: "0.5 gal"},
+            {item: "pepsi", count: "2 L"}
+          ]
+        }
+      ];
+
+
     });
