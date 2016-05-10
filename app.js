c52a06d064d16cafd6227794d4aa3ba3b11b1672
Edward Colosky <edwardcolosky628@gmail.com>
2016-05-10 18:07:27 -0400

    0.5.0 - nutrition page added

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
                 controller : 'ModalDemoCtrl'
             })
             // route for the chapter history
-            .when('/ChapterHistory', {
-                templateUrl : 'pages/ChapterHistory/index.html'
+            .when('/nutrition', {
+                templateUrl : 'pages/nutrition.html',
+                controller: 'nutCtrl'
             })
             // route for the stories page
             .when('/recipes', {
@@ -44,7 +45,7 @@ var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
         { title:"Inventory", href:"#/inventory", icon: 'glyphicon glyphicon-list' },
         { title:"Shopping List", href:"#/list" ,icon: 'glyphicon glyphicon-shopping-cart' },
         { title:"Add Item", href:"#/add" ,icon: 'glyphicon glyphicon-plus' },
-        { title:"Nuitrition", href:"#/nuitrition", icon: 'glyphicon glyphicon-scale'  },
+        { title:"Nutrition", href:"#/nutrition", icon: 'glyphicon glyphicon-scale'  },
         { title:"Recipes", href:"#/recipes", icon: 'glyphicon glyphicon-cutlery'  }
       ];
       $scope.changeTab = function(data) {
@@ -80,6 +81,28 @@ var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
       ]
     });
 
+    app.controller('nutCtrl', function($scope){
+      $scope.groups = [
+        {name: 'Calories:', percent: 1550,max:2000, type: 'info', unit: 'Cal'},
+        {name: 'Fat:', percent: 60,max:70, type: 'danger',unit: 'g'},
+        {name: 'Carbs:', percent: 297,max:310, type: 'danger',unit: 'g'},
+        {name: 'Protein:', percent: 45, max:50,type: 'success',unit: 'g'},
+        {name: 'Sodium:', percent: 1.17,max:2.3, type: 'info',unit: 'g'},
+        {name: 'Dietary Fiber:', percent: 20,max: 30, type: 'info',unit: 'g'},
+        {name: 'Sugars:', percent: 47,max: 90, type: 'info',unit: 'g'}
+      ]
+      $scope.groupsW = [
+        {name: 'Calories:', percent: 41,max:50, type: 'success', unit: 'Cal'},
+        {name: 'Fat:', percent: 37,max:50, type: 'success',unit: 'g'},
+        {name: 'Carbs:', percent: 52,max:50, type: 'warning',unit: 'g'},
+        {name: 'Protein:', percent: 52, max:50,type: 'success',unit: 'g'},
+        {name: 'Sodium:', percent: 65,max:50, type: 'warning',unit: 'g'},
+        {name: 'Dietary Fiber:', percent: 50,max: 50, type: 'info',unit: 'g'},
+        {name: 'Sugars:', percent: 50,max: 50, type: 'info',unit: 'g'}
+      ]
+    });
+
+
     //add controller
     app.controller('ModalDemoCtrl',function($scope, $uibModal, $log){
       // modal instance created
@@ -97,7 +120,7 @@ var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
     });
     //modal controller for the instance
     app.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, $log){
-      $scope.cats = ['fruits', 'vegetables', 'meats', 'beverage', 'bakery', 'baking supplies'];
+      $scope.cats = ['fruit', 'vegetable', 'meat', 'beverage', 'bakery', 'baking supply','candy','grain'];
       $scope.temp = $scope.cats[0];
       $scope.item = {
         name: '',
@@ -149,27 +172,26 @@ var app = angular.module('app', ['ui.bootstrap','ngRoute','ngAnimate']);
     //shopping list controller
     app.controller('listCtrl',function($scope){
       $scope.list = [
-        {head: "Produce",
+        {head: "Grain",
           items: [
-            {item: "bannannas", count: "6"},
-            {item: "oranges",class: "-success" ,count: "1"},
-            {item: "strawberries", count: "1 Qt."}
+            {item: "brown rice", count: "1 x 2.5 lbs bag"},
+            {item: "white bread" ,count: "1 x 24 slice loaf"},
+            {item: "linguine pasta", count: "1 x 1 lbs box"},
+            {item: "quinoa", count: "1 x 2.5 lbs bag"}
           ]
         },
-        {head: "Meat",
+        {head: "Candy",
           items: [
-            {item: "chicken",class: "" ,count: "5 lbs"},
-            {item: "pork tenderloin", count: "3 lbs"},
-            {item: "bacon", count: "1.5 lbs"},
-            {item: "chicken sausage",class: "-success" , count: "5 links"}
+            {item: "hershey chocolate",count: "2 x 46 g bar"},
+            {item: "peppermint candy", count: "1 x 340 g bag"}
           ]
         },
-        {head: "Beverages",
+        {head: "Baking Supplies",
           items: [
-            {item: "beer", count: "2.5 gal"},
-            {item: "wine", count: "2.25 L"},
-            {item: "milk", count: "0.5 gal"},
-            {item: "pepsi", count: "2 L"}
+            {item: "granulated sugar", count: "1 x 5 lbs bag"},
+            {item: "all-purpose flour", count: "1 x 5 lbs bag"},
+            {item: "baking soda", count: "1 x 1 lb box"},
+            {item: "baking powder", count: "1 x 284 g can"}
           ]
         }
       ];
